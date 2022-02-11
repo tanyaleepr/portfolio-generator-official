b505763d4100bda091a43252708543bf529b5605
tanyaleepr <tanyaleepr@gmail.com>
2022-02-11 16:28:28 -0600

    generate about me and sections

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const fs = require('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
@@ -129,11 +129,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
+    const pageHTML = generatePage(portfolioData);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
-  });
+    console.log('Page created! Check out index.html in this directory to see it!');
+     });
+  });   
